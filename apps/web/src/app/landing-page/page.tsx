014ac42b4fa6aea6cff115bddb77372e18e6fed8
Dan Sever <dansever96@gmail.com>
2025-08-20 23:50:21 +0300

    Refactor LandingPage component by removing unused children prop for improved clarity and simplicity.

--- apps/web/src/app/landing-page/page.tsx
+++ apps/web/src/app/landing-page/page.tsx
@@ -2,11 +2,10 @@ import { AnimatedGridBackground } from './_components/AnimatedGridBackground';
 import { GradientBackground } from './_components/GradientBackground';
 import LandingPageClient from './ClientPage';
 
-export default function LandingPage({ children }: { children: React.ReactNode }) {
+export default function LandingPage() {
   return (
     <div className="relative min-h-screen z-10 bg-muted">
       <GradientBackground />
-
       <AnimatedGridBackground />
       <div className="max-w-7xl mx-auto">
         <LandingPageClient />
