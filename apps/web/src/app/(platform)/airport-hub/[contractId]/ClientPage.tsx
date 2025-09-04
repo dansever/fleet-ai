f610462a4eece56b3bd604691364250a0394b6f2
Dan Sever <dansever96@gmail.com>
2025-09-04 18:31:21 +0300

    Update ClientPage Layout for Improved Spacing
    - Adjusted the gap between elements in the ClientPage component from 2 to 4 for better visual separation and improved user experience.

--- apps/web/src/app/(platform)/airport-hub/[contractId]/ClientPage.tsx
+++ apps/web/src/app/(platform)/airport-hub/[contractId]/ClientPage.tsx
@@ -32,7 +32,7 @@ export default function ContractClientPage() {
 
   const Main = () => {
     return (
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-4">
         <MainCard title="Contract Information" icon={<FileText />}>
           <div className="grid grid-cols-2 gap-4">
             <div>
